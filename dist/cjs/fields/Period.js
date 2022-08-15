d529c8aad4f2e1523999f953e2ed3ba438269f4a
unitx.st-ipc <unitx.st-ipc>
2022-08-15 14:55:47 -0700

    more BOX

--- dist/cjs/fields/Period.js
+++ dist/cjs/fields/Period.js
@@ -5,7 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = Period;
 
-var _Box = _interopRequireDefault(require("@material-ui/core/Box/Box"));
+var _Box = _interopRequireDefault(require("@material-ui/core/Box"));
 
 var _Typography = _interopRequireDefault(require("@material-ui/core/Typography"));
 
